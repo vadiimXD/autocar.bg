f09eacf63bb3b5d9f448cc80733062749703c48c
vadiimXD <vadim123bg@gmail.com>
2024-04-09 13:33:03 +0300

    added footer

--- autocar/src/app/app.module.ts
+++ autocar/src/app/app.module.ts
@@ -3,6 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AuthModule } from './auth/auth.module';
+import { CoreModule } from './core/core.module';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
@@ -10,6 +13,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    AuthModule,
+    CoreModule,
+    SharedModule,
     AppRoutingModule
   ],
   providers: [],
