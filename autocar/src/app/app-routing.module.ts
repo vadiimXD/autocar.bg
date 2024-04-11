fdf43ba26d46872604de09f74c25984f40226aa2
vadiimXD <vadim123bg@gmail.com>
2024-04-11 13:49:49 +0300

    added register page

--- autocar/src/app/app-routing.module.ts
+++ autocar/src/app/app-routing.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './public/home/home.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { LoginComponent } from './auth/login/login.component';
 
 const routes: Routes = [
-  { path: "", component: HomeComponent }
+  { path: "", component: HomeComponent },
+  { path: "register", component: RegisterComponent },
+  { path: "login", component: LoginComponent },
 ];
 
 @NgModule({
--- autocar/src/app/auth/auth.module.ts
+++ autocar/src/app/auth/auth.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    RegisterComponent,
+    LoginComponent
+  ],
   imports: [
     CommonModule
-  ]
+  ],
+  exports: [RegisterComponent, LoginComponent]
 })
 export class AuthModule { }
